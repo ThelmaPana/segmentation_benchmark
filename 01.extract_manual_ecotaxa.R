a62c9eaddf8666c7a039a2bf4ba57498854779d9
Thelma Panaiotis <thelma@crans.org>
2021-10-28 09:30:10 +0000

    Locate manual particles in avi files

--- 01.extract_manual_ecotaxa.R
+++ 01.extract_manual_ecotaxa.R
@@ -7,14 +7,22 @@
 
 library(tidyverse)
 library(ecotaxar)
+library(reticulate)
+library(parallel)
+
 
 # Set output directory
-#output_dir <- "data/manual"
-output_dir <- "data_cc4/manual"
+output_dir <- "data/manual"
+#output_dir <- "data_cc4/manual"
 
+n_cores <- 12
+
+
+## Extract particles from ecotaxa ----
+#--------------------------------------------------------------------------#
 # project number
-#projid <- as.integer(4520) # for CC7
-projid <- as.integer(4693) # for CC4 
+projid <- as.integer(4520) # for CC7
+#projid <- as.integer(4693) # for CC4 
 
 # connect to db
 db <- db_connect_ecotaxa()
@@ -38,10 +46,81 @@ obj <- tbl(db, "objects") %>%
     ) %>% 
   select(-classif_id)
 
+# disconnect from db
+db_disconnect_ecotaxa(db)
+
+# ignore objects which are detritus or othertocheck
+obj <- obj %>% filter(!taxon %in% c("detritus", "othertocheck"))
+
+
+## Compute particles position in avi files ----
+#--------------------------------------------------------------------------#
+# Read apeep particles props, get img name, avi file and frame nb
+# Do this with pandas
+
+# List all tar archives
+tar_files <- list.files(path = "data/regular_apeep_def/particles/", pattern = ".tar", full.names = TRUE, recursive = TRUE)
+
+# Import python package
+pd <- import("pandas")
+tarfile <- import("tarfile")
+
+
+# Parallel read of tsv files
+reads <- mclapply(tar_files, function(file) {
+  
+  # Open tar archive
+  arch <-  tarfile$open(file, mode = "r")
+  
+  # Get name of tsv file
+  tsv_file_name <- arch$getnames() %>% str_subset(".tsv")
+  
+  # Read tsv file
+  df <- pd$read_csv(arch$extractfile(tsv_file_name), sep = "\t", comment = "[") %>% 
+    as_tibble() %>% 
+    select(img_name = acq_id, avi_file = object_avi_file, frame_nb = object_frame_nb) %>% unique()
+  
+  # Close tar archive
+  arch$close()
+  
+  return(df)
+}, mc.cores=n_cores) 
+# this returns a list, recombine it into a tibble
+avi_files <- do.call(bind_rows, reads) 
+
+# Compute relative frame within each image
+avi_files <- avi_files %>% 
+  arrange(avi_file, frame_nb) %>% 
+  group_by(avi_file) %>% 
+  mutate(frame_img = row_number() - 1) %>% 
+  ungroup()
+
+
+# Compute relative frame number and line number and join with avi file and true frame number
+obj <- obj %>% 
+  mutate(
+    frame_img = bbox1 %/% 2048,  # frame number as bbox1 // 2048
+    line_nb = bbox1 %% 2048, # line number as bbox1 modulo 2048
+  ) %>%  
+  left_join(avi_files, by = c("img_name", "frame_img")) %>% # join with avi file
+  select(-frame_img) %>% 
+  arrange(img_name, bbox1)
+
+
+# Check consistency
+obj %>% 
+  filter(avi_file == unique(obj$avi_file)[1]) %>% 
+  ggplot() +
+  geom_point(aes(x = bbox1, y = line_nb, color = factor(frame_nb))) +
+  geom_hline(yintercept = 2048) +
+  xlim(0, 10240) + ylim(0, 2048)
+
+
+## Save particle properties ----
+#--------------------------------------------------------------------------#
 # write a csv 
 obj %>% write_csv(file = file.path(output_dir, "ecotaxa_export_test_set.csv"))  
 
-# disconnect from db
-db_disconnect_ecotaxa(db)
+
 
 
